d108fdd0a81e39cebb15936a39bcbf4645db7775
ali-shah007 <alishah69.ah@gmail.com>
2024-05-26 03:17:46 +0500

    cahnge mobile

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -53,13 +53,13 @@ function Contact() {
     };
     
     return (
-        <div name="contact" className='w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white'>
-            <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full'>
+        <div name="contact" className='w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white flex flex-col'>
+            <div className='max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full w-full'>
                 <div className='flex flex-col items-center justify-center'>
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Contact</p>
                     <p className='py-6'>Submit the form below to get in touch with me</p>
                 </div>
-                <div className='flex justify-center items-center'>
+                <div className='flex justify-center items-center w-full'>
                     <form ref={form} onSubmit={sendEmail} className='flex flex-col w-full md:w-1/2'>
                         <input type="text" name="name" placeholder='Enter your name' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
                         <input type="email" name="email" placeholder='Enter your email' className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
